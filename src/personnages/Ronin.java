f3538971c957ce3160c8c5a3e74f1c130733c2a3
unknown <bermobiz32@gmail.com>
2022-11-15 11:32:24 +0100

    Duel entre Ronin et Yakuza, fin du TP 4!

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -1,7 +1,7 @@
 package personnages;
 
 public class Ronin extends Humain {
-	private int attributHonneur=1;
+	private int honneur=1;
 	
 	public Ronin(String nom, String boissonFavorite, int quantiteArgent) {
 		super(nom, boissonFavorite, quantiteArgent);
@@ -11,5 +11,20 @@ public class Ronin extends Humain {
 		quantiteArgent-=argentDonne;
 		parler(beneficiaire.getNom()+" prend ces "+argentDonne+" sous.");
 		beneficiaire.recevoir(argentDonne);
+	}
+	public void provoquer(Yakuza adversaire) {
+		int force=2*honneur;
+		if (force>= adversaire.getReputation()) {
+			parler("Je t'ai retrouvé vermine, tu vas payer pour ce que tu as fait à ce pauvre marchand!");
+			quantiteArgent+=adversaire.quantiteArgent;
+			honneur+=1;
+			parler("Je t'ai eu petit yakusa!");
+			adversaire.perdre();
+		}else {
+			honneur-=1;
+			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse ont en pris un coup.");
+			adversaire.gagner(quantiteArgent);
+			quantiteArgent=0;
+		}
 	}
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -2,8 +2,10 @@ package personnages;
 
 public class Yakuza extends Humain{
 	private int reputation=0;
+	private String clan;
 	public Yakuza(String nom, String boissonFavorite, int quantiteArgent, String clan) {
 		super(nom, boissonFavorite, quantiteArgent);
+		this.clan=clan;
 	}
 	public void extorquer(Commercant victime) {
 		int argentVictime;
@@ -13,7 +15,25 @@ public class Yakuza extends Humain{
 		argentVictime=victime.quantiteArgent;
 		victime.seFaireExtorquer();
 		quantiteArgent+=argentVictime;
-		reputation+=1;
+		setReputation(getReputation() + 1);
 		parler("J'ai piqué les "+argentVictime+" sous de "+nomVictime+" ce qui me fait "+quantiteArgent+" sous dans ma poche. Hi! Hi!");
+	}
+	public int perdre() {
+		setReputation(getReputation() - 1);
+		parler("J'ai perdu mon duel et mes "+quantiteArgent+" sous, snif... J'ai déshonoré le clan de "+clan);
+		quantiteArgent=0;
+		return 0;
+	}
+	public int gagner(int gain) {
+		quantiteArgent+=gain;
+		setReputation(getReputation() + 1);
+		parler("Ce ronin pensait vraiment battre "+getNom()+" du clan de "+clan+" ? Je l'ai dépouillé de ses "+gain+" sous.");
+		return 0;
+	}
+	public int getReputation() {
+		return reputation;
+	}
+	public void setReputation(int reputation) {
+		this.reputation = reputation;
 	}
 }
